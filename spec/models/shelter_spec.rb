2d88cb681c20b71a4f84f6f91b170eaeea2a1a11
Victor Sanchez <vsanantone@gmail.com>
2023-12-11 12:27:59 -0600

    fix: delete testing

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -30,9 +30,25 @@ RSpec.describe Shelter, type: :model do
         expect(Shelter.reverse_alpha_order).to_not eq([@shelter_2, @shelter_2, @shelter_1])
       end
     end
-    describe "#search" do
-      it "returns partial matches" do
-        expect(Shelter.search("Fancy")).to eq([@shelter_3])
+
+    describe "pending_applications" do
+      it "returns all shelters with at least one pending application" do
+        victor_app = Application.create!(applicant_name: "Victor Antonio Sanchez", address: "97 Jaffa Road", city: "Jerusalem", state: "Israel", zip_code: "9103401", application_status: "Pending",description: "Because I'm rich! :)")
+        sam_app = Application.create!(applicant_name: "Sam Puttman", address: "94 Jaffa Road", city: "Jerusalem", state: "Israel", zip_code: "9103401",application_status: "Pending", description: "Because I'm super rich! :)")
+        natalie_app = Application.create!(applicant_name: "Natalie Lan", address: "96 Jaffa Road", city: "Jerusalem", state: "Israel", zip_code: "9103401", description: "Because I'm super deduperdy rich! :)")
+
+        shelter_1 = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 3)
+        shelter_2 = Shelter.create!(name: "Denver shelter", city: "Denver, CO", foster_program: false, rank: 5)
+        shelter_3 = Shelter.create!(name: "Boulder shelter", city: "Boulder, CO", foster_program: false, rank: 9)
+
+        pet_1 = shelter_1.pets.create!(name: "Roxy", breed: "labrador", age: 7, adoptable: true)
+        pet_2 = shelter_2.pets.create!(name: "Rocky", breed: "dalmation", age: 4, adoptable: true) 
+        pet_3 = shelter_3.pets.create!(name: "Brownie", breed: "ewok", age: 12, adoptable: true)
+
+        victor_app.pets << pet_1
+        sam_app.pets << pet_2
+        natalie_app.pets << pet_3
+        expect(Shelter.pending_applications).to eq([shelter_1, shelter_2])
       end
     end
 
